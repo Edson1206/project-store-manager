3f53582bfea647ebed8f4158905c06a1f41fee70
Edson Pereira <ed.1206@gmail.com>
2022-11-14 13:17:03 -0300

    Craição dos arquivos da pasta models para o requisito 1

---
+++ src/models/product.model.js
@@ -0,0 +1,23 @@
+const connection = require('./connection');
+
+const findAll = async () => {
+  const [result] = await connection.execute(
+    'SELECT * FROM StoreManager.products',
+  );
+  return result;
+};
+
+const findById = async (productId) => {
+  const [[product]] = await connection.execute(
+    'SELECT * FROM StoreManager.products WHERE id = ?',
+    [productId],
+  );
+  return (product);
+};
+const insert = () => { };
+
+module.exports = {
+  findAll,
+  findById,
+  insert,
+};
